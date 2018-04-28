e1ed9d9970f6e5f48d611b34338731c07fabf68c
ElenaReivytyte <31854424+ElenaReivytyte@users.noreply.github.com>
2018-04-28 22:47:13 +0300

    Add files via upload

---
+++ IrcBot/Irc.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace IrcBot
+{
+    class Irc
+    {
+        public string Hostname { get; }
+        public string ServerAdress { get; set; }
+        public int Port { get; }
+        public string Nickname { get; set; }
+        public string Realname { get; }
+        public bool Connected { get; }
+
+        TcpClient tcpClient;
+        StreamReader inputStream;
+        StreamWriter outputStream;
+
+        Queue<string> MessageQueue = new Queue<string>();
+
+        bool QueueIsRunning = false;
+        int QueueTimer = 800;
+
+        public Irc(string hostname, int port, string nickname, string realname)
+        {
+            Hostname = hostname;
+            Port = port;
+            Nickname = nickname;
+            Realname = realname;
+
+            try
+            {
+                tcpClient = new TcpClient(Hostname, Port);
+                inputStream = new StreamReader(tcpClient.GetStream());
+                outputStream = new StreamWriter(tcpClient.GetStream());
+                outputStream.Flush();
+
+                outputStream.WriteLine("NICK " + Nickname);
+                outputStream.WriteLine("USER " + Realname + " 8 * :" + Realname);// The USER message is used at the beginning of connection 
+                                                                                 //to specifythe username, hostname, servername and realname of s new user.
+                outputStream.Flush();
+
+                Connected = true;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Connected = false;
+            }
+
+        }
+
+        public string readData()
+        {
+            try
+            {
+                return inputStream.ReadLine();
+
+            }
+            catch (Exception ex)
+            {
+                return "Could not read data: " + ex.Message;
+            }
+        }
+        public void sendData(string Data)
+        {
+            try
+            {
+                outputStream.WriteLine(Data);
+                outputStream.Flush();
+                Console.WriteLine("<" + Data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not send data" + ex.Message);
+            }
+
+        }
+
+        public void joinRoom(string message)
+        {
+            if (Regex.IsMatch(message, "#([a-zA-Z0-9]+)"))
+            {
+                sendData("JOIN " + message);
+            }
+            else Console.WriteLine("No such channel");
+
+        }
+        public void ChatMessage(string message, string i)
+        {
+            //PRIVMSG (privatemessage)
+            string Message = "PRIVMSG " + i + " :" + message;
+            MessageQueue.Enqueue(Message);
+
+            if (!QueueIsRunning)
+            {
+                sendChatMessage();
+            }
+
+        }
+        private void sendChatMessage()
+        {
+            foreach (string i in MessageQueue)
+            {
+                QueueIsRunning = true;
+                outputStream.WriteLine(i);
+                outputStream.Flush();
+                System.Threading.Thread.Sleep(QueueTimer);
+
+            }
+            MessageQueue.Clear();
+            QueueIsRunning = false;
+        }
+
+        public void listNames(string channel)
+        {
+
+            if (Regex.IsMatch(channel, "#([a-zA-Z0-9]+)"))
+            {
+                sendData("NAMES " + channel);
+            }
+            else
+            {
+                sendData("NAMES ");
+            }
+
+        }
+
+        public void listChannels(string channel)
+        {
+
+            if (Regex.IsMatch(channel, "#([a-zA-Z0-9]+)"))
+            {
+                sendData("LIST " + channel);
+            }
+            else
+            {
+                sendData("LIST ");
+            }
+
+        }
+
+        public void quitSession()
+        {
+            sendData("QUIT " + ":Bye!");
+        }
+        public void serverStats(string queries)
+        {
+            if (Regex.IsMatch(queries, @"[chiklmoyu]"))
+            {
+                sendData("STATS " + queries);
+            }
+            else Console.WriteLine("Need to put stats query (c, h, i, k, l, m, o, y, u)");
+
+        }
+        public void commandWho(string mess)
+        {
+            sendData("WHO " + mess);
+        }
+        public void commandWhois(string mess)
+        {
+            sendData("WHOIS " + mess);
+        }
+        public void partChannel(string mess)
+        {
+            if (Regex.IsMatch(mess, "#([a-zA-Z0-9]+)"))
+            {
+                sendData("PART " + mess);
+            }
+            else Console.WriteLine("Channel not specified");
+        }
+        public void newNick(string mess)
+        {
+            sendData("NICK " + mess);
+        }
+        public void serverInfo()
+        {
+            sendData("INFO " + ServerAdress);
+        }
+    }
+}
---
+++ IrcBot/IrcBot.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace IrcBot
+{
+
+    class IrcBot
+    {
+        Irc IrcConnection;
+        string channel = null;
+        bool input = true;
+        string nickName;
+
+        static void Main(string[] args)
+        {
+            IrcBot Program = new IrcBot();
+            Program.GetData();
+        }
+        private void TimerCallback(Object o)
+        {
+            string Data = IrcConnection.readData();
+            if (Data != null)
+            {
+                Console.WriteLine(Data);
+            }
+
+        }
+        private void GetData()
+        {
+
+            IrcConnection = new Irc("shell.riftus.lt", 6667, "prataVetra_123", "ElenaReivytyte");
+            if (!IrcConnection.Connected)
+            {
+                Console.WriteLine("Cannot connect");
+                Console.ReadKey();
+                return;
+            }
+            while (true)
+            {
+                string Data = IrcConnection.readData();
+
+                if (Data != null)
+                {
+                    Console.WriteLine(Data);
+
+                    if (Regex.IsMatch(Data, @":(.+) ([0-9]+) " + IrcConnection.Nickname + " :End of /MOTD command."))
+                    {
+                        string[] tRegEx = Regex.Split(Data, @":(.+) ([0-9]+) " + IrcConnection.Nickname + " :End of /MOTD command.");
+                        IrcConnection.ServerAdress = tRegEx[1];
+                        Messages();
+
+                    }
+
+                }
+
+            }
+
+        }
+        private void ChannelData()
+        {
+            while (true)
+            {
+                string Data = IrcConnection.readData();
+                if (Data != null)
+                {
+                    Console.WriteLine(Data);
+
+                    if (Regex.IsMatch(Data, @":End of /NAMES list."))
+                    {
+                        Messages();
+
+                        break;
+                    }
+                    else
+                    if (Regex.IsMatch(Data, @":End of /LIST"))
+                    {
+                        Messages();
+                        break;
+                    }
+                    else if (Regex.IsMatch(Data, @":End of /STATS report"))
+                    {
+                        Messages();
+                        break;
+                    }
+                    else if (Regex.IsMatch(Data, @":End of /WHO list."))
+                    {
+                        Messages();
+                        break;
+                    }
+                    else if (Regex.IsMatch(Data, @":End of /WHOIS list."))
+                    {
+                        Messages();
+                        break;
+                    }
+                    else if (Regex.IsMatch(Data, @"PART " + channel))
+                    {
+                        Messages();
+                        break;
+                    }
+                    else if (Regex.IsMatch(Data, @"NICK :" + nickName))
+                    {
+                        Messages();
+                        break;
+                    }
+                    else if (Regex.IsMatch(Data, @":End of /INFO list."))
+                    {
+                        Messages();
+                        break;
+                    }
+
+                }
+            }
+
+        }
+        private void Messages()
+        {
+            string command;
+            string mess;
+            bool a = true;
+            while (a)
+            {
+                command = Console.ReadLine();
+                if (Regex.IsMatch(command, @"/*"))
+                {
+                    switch (command)
+                    {
+                        case "/NAMES":    //user can list all nicknames that are visible to them on any channel that they can see
+                            Console.WriteLine("Enter channel or press enter");
+                            mess = Console.ReadLine();
+                            IrcConnection.listNames(mess);
+                            ChannelData();
+                            break;
+                        case "/LIST": //The list message is used to list channels and their topics.
+                            Console.WriteLine("Enter channel or press enter");
+                            mess = Console.ReadLine();
+                            IrcConnection.listChannels(mess);
+
+                            ChannelData();
+                            break;
+                        case "/QUIT"://A client session is ended with a quit message.                          
+                            input = false;
+                            a = false;
+                            IrcConnection.quitSession();
+                            break;
+                        case "/PRIVMSG"://PRIVMSG is used to send private messages between users.
+                            if (channel != null)
+                            {
+                                Chat();
+                            }
+                            else Console.WriteLine("Connect to channel");
+                            break;
+
+                        case "/STATS": //The stats message is used to query statistics of certain server.
+                            Console.WriteLine("Enter stats query (c, h, i, k, l, m, o, y, u)");
+                            mess = Console.ReadLine();
+                            IrcConnection.serverStats(mess);
+                            ChannelData();
+                            break;
+                        case "/JOIN":// The JOIN command is used by client to start listening a specificchannel.
+                            Console.WriteLine("Enter Channel");
+                            mess = Console.ReadLine();
+                            IrcConnection.joinRoom(mess);
+                            channel = mess;
+                            ChannelData();
+                            break;
+                        case "/WHO"://returns a list of information which 'matches' the<name> parameter given by the client.
+                            Console.WriteLine("Enter name");
+                            mess = Console.ReadLine();
+                            IrcConnection.commandWho(mess);
+                            ChannelData();
+                            break;
+                        case "/WHOIS"://This message is used to query information about particular user.
+                            Console.WriteLine("Enter name");
+                            mess = Console.ReadLine();
+                            IrcConnection.commandWhois(mess);
+                            ChannelData();
+                            break;
+                        case "/PART": //removed from the list of active users for all given channels listed in theparameter string.
+                            Console.WriteLine("Enter Channel");
+                            mess = Console.ReadLine();
+                            IrcConnection.partChannel(mess);
+                            ChannelData();
+                            break;
+                        case "/NICK"://NICK message is used to give user a nickname or change the previous one.
+                            Console.WriteLine("//Enter new nickname");
+                            mess = Console.ReadLine();
+                            IrcConnection.newNick(mess);
+                            nickName = mess;
+                            ChannelData();
+                            break;
+                        case "/INFO":
+                            IrcConnection.serverInfo();
+                            ChannelData();
+                            break;
+
+                    }
+
+                }
+
+            }
+        }
+
+        private void Chat()
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 2000);
+            Console.WriteLine("Enter your message");
+            string message = Console.ReadLine();
+            IrcConnection.ChatMessage(message, channel);
+            t.Change(Timeout.Infinite, Timeout.Infinite);
+
+        }
+
+
+
+    }
+}
